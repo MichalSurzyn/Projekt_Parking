bd76b7351288ce59abdb89df8dd27741f3ef026e
Michal Surzyn <michal.surzyn2002@gmail.com>
2025-01-09 17:50:04 +0100

    added filtr by city on mainpage

--- backend/php/get_cities.php
+++ backend/php/get_cities.php
@@ -13,36 +13,23 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Pobranie parametru ID miasta, jeśli jest podany
-$city_id = isset($_GET['city_id']) ? (int)$_GET['city_id'] : 0;
-
-// Zapytanie SQL do pobrania parkingów z filtrem lub bez
-if ($city_id > 0) {
-    $sql = "SELECT ID_Parkingu, Nazwa, Lokalizacja, Liczba_Miejsc, Typ FROM Parking WHERE ID_Miasta = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("i", $city_id);
-} else {
-    $sql = "SELECT ID_Parkingu, Nazwa, Lokalizacja, Liczba_Miejsc, Typ FROM Parking";
-    $stmt = $conn->prepare($sql);
-}
-
-$stmt->execute();
-$result = $stmt->get_result();
+// Zapytanie SQL do pobrania miast
+$sql = "SELECT ID_Miasta, Nazwa FROM Miasto";
+$result = $conn->query($sql);
 
 // Tablica na wyniki
-$parkings = [];
+$cities = [];
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        $parkings[] = $row;
+        $cities[] = $row;
     }
 }
 
 // Zwracanie danych w formacie JSON
 header('Content-Type: application/json');
-echo json_encode($parkings);
+echo json_encode($cities);
 
 // Zamknięcie połączenia
-$stmt->close();
 $conn->close();
 ?>
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,20 +1,22 @@
 // Wywołanie na starcie
-fetchAndDisplayParkings()
-setInterval(fetchAndDisplayParkings, 5000)
-
+/*
+fetchAndDisplayCities();
+fetchAndDisplayParkings();
+setInterval(fetchAndDisplayParkings, 5000);
+*/
 document
-  .getElementById('parking-ul')
-  .addEventListener('click', function (event) {
-    if (event.target.tagName === 'LI') {
+  .getElementById("parking-ul")
+  .addEventListener("click", function (event) {
+    if (event.target.tagName === "LI") {
       // Pobierz nazwę parkingu
-      const parkingName = event.target.textContent.split(' - ')[0]
-      document.getElementById('reservation-parking-name').textContent =
-        parkingName
+      const parkingName = event.target.textContent.split(" - ")[0];
+      document.getElementById("reservation-parking-name").textContent =
+        parkingName;
 
       // Wywołaj funkcję pobierającą pojazdy użytkownika i wypełniającą pole wyboru
-      loadUserVehicles()
+      loadUserVehicles();
 
       // Pokaż formularz rezerwacji
-      document.getElementById('reservation-form').classList.remove('hidden')
+      document.getElementById("reservation-form").classList.remove("hidden");
     }
-  })
+  });
